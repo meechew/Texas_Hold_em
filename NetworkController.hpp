c6b7c5afd9d59cbff0f74f629bd4275335fa9eed
meechew <cbunt@pdx.edu>
2020-05-22 18:32:36 -0700

    getting started on NetworkController.hpp

--- NetworkController.hpp
+++ NetworkController.hpp
@@ -1,13 +1,21 @@
 // Created by CBunt on 20 May 2020.
 //
+// Documentation for this controller can be found at:
+// https://www.boost.org/doc/libs/1_73_0/doc/html/boost_asio.html
 
 #ifndef TEXAS_HOLD_EM_NETWORKCONTROLLER_HPP
 #define TEXAS_HOLD_EM_NETWORKCONTROLLER_HPP
+#include <iostream>
 #include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
 
 class NetworController {
 
 };
 
+class session {
+
+};
 
 #endif //TEXAS_HOLD_EM_NETWORKCONTROLLER_HPP
