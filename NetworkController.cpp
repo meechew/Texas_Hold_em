f51b1b63fcd04b3365aef76bc6f5ab43a183e5ec
meechew <cbunt@pdx.edu>
2020-05-24 02:55:15 -0700

    getting started on NetworkController.hpp

--- NetworkController.cpp
+++ NetworkController.cpp
@@ -2,3 +2,34 @@
 //
 
 #include "NetworkController.hpp"
+
+void NetworkController::do_accept() {
+  acceptor_.async_accept(
+    [this](boost::system::error_code ec, tcp::socket socket) {
+      if (!ec) {
+        std::make_shared<Session>(std::move(socket))->start();
+      }
+      do_accept();
+    });
+}
+
+void Session::do_read() {
+  auto self(shared_from_this());
+  socket_.async_read_some(boost::asio::buffer(data_, max_length),
+    [this, self](boost::system::error_code ec, std::size_t length) {
+      if (!ec) {
+        do_write(length);
+      }
+    });
+}
+
+void Session::do_write(std::size_t length) {
+  auto self(shared_from_this());
+  boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+    [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+      if (!ec) {
+       do_read();
+      }
+    });
+}
+
--- NetworkController.hpp
+++ NetworkController.hpp
@@ -10,12 +10,30 @@
 
 using boost::asio::ip::tcp;
 
-class NetworController {
+class Session: public std::enable_shared_from_this<Session> {
+public:
+  Session(tcp::socket socket)
+      : socket_(std::move(socket)) {}
+  void start() {do_read();}
+private:
+  tcp::socket socket_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+
+  void do_read();
+  void do_write(std::size_t length);
 
 };
 
-class session {
+class NetworkController {
+public:
+  NetworkController(boost::asio::io_context& io_context, short port)
+      : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+        {do_accept();}
 
+private:
+  void do_accept();
+  tcp::acceptor acceptor_;
 };
 
 #endif //TEXAS_HOLD_EM_NETWORKCONTROLLER_HPP
