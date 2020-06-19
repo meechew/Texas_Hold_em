ce5484a1258ae45d6b16863db306c4a42235bf92
meechew <cbunt@pdx.edu>
2020-06-18 21:54:11 -0700

    need to remove bimap and use pointers.

--- TexasServer.hpp
+++ TexasServer.hpp
@@ -3,15 +3,15 @@
 
 #ifndef TEXAS_HOLD_EM_SERVER_HPP
 #define TEXAS_HOLD_EM_SERVER_HPP
-#include <stdexcept>
-#include <iostream>
+#include <deque>
+#include <cstdio>
+#include <memory>
+#include <chrono>
 #include <utility>
 #include <cstdlib>
 #include <cstring>
-#include <memory>
-#include <chrono>
-#include <cstdio>
-#include <deque>
+#include <iostream>
+#include <stdexcept>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <boost/format.hpp>
@@ -19,11 +19,11 @@
 #include <boost/container/map.hpp>
 #include <boost/container/vector.hpp>
 #include <boost/container/string.hpp>
-#include <boost/container/container_fwd.hpp>
 #include <boost/range/algorithm/sort.hpp>
+#include <boost/container/container_fwd.hpp>
 #include <boost/range/algorithm/random_shuffle.hpp>
 #include <boost/interprocess/streams/bufferstream.hpp>
 
 
 
-#EndIf
+#endif
